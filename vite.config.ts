6ca0a173679be4b312e6497ee414f2dbca07c6e5
JJaks <jaagopj@gmail.com>
2025-07-10 18:09:00 +0300

    BFU-39: Get app version from package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
+// Read package.json synchronously at build time
+const packageJson = await import('./package.json', { with: { type: 'json' } });
+
 export default defineConfig({
 	plugins: [
 		enhancedImages(),
 		sveltekit()
-	]
+	],
+	define: {
+		__APP_VERSION__: JSON.stringify(packageJson.default.version)
+	}
 });
