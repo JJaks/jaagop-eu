e750a67fa68559c302dde8bbe5b562f30f7dba68
JJaks <dev@jaagop.eu>
2025-09-30 02:04:09 +0300

    Add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
 	import '../styles/index.css';
 
+	injectAnalytics();
 	injectSpeedInsights();
 </script>
 
